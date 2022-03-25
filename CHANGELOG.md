dde74cdb272d5717789c94592b2b1379ad5efc6b
Bob Bloom <bob.bloom@lasallesoftware.ca>
2022-03-25 11:58:09 -0400

    Prepare for v2.3.0 release. #32

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 ## VERSION 2.2.4 (coming!)
 
+## VERSION 2.3.0 (March 25, 2022)
+* [v2.3.0 issues](https://github.com/LaSalleSoftware/ls-lasalleuifrontend-pkg/milestone/11?closed=1)
+
 ## VERSION 2.2.3 (January 20, 2022)
 * [v2.2.3 issues](https://github.com/LaSalleSoftware/ls-lasalleuifrontend-pkg/milestone/10?closed=1)
 
--- src/Version.php
+++ src/Version.php
@@ -29,14 +29,14 @@ class Version
      *
      * @var string
      */
-    const VERSION = '2.2.3';
+    const VERSION = '2.3.0';
 
     /**
      * This package's release date.
      *
      * @var string
      */
-    const RELEASEDATE = 'January 20, 2022';
+    const RELEASEDATE = 'March 25, 2022';
 
     /**
      * This package's name.
